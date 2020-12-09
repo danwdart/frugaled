8f7541e407844cbd0698821f6609427f3fa5d442
Dan Dart <git@dandart.co.uk>
2020-12-09 22:56:10 +0000

    deps

--- frugaled.nix
+++ frugaled.nix
@@ -1,10 +1,12 @@
-{ mkDerivation, base, discord-haskell, stdenv }:
+{ mkDerivation, base, discord-haskell, postgresql-simple, stdenv }:
 mkDerivation {
   pname = "frugaled";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base discord-haskell ];
+  executableHaskellDepends = [
+    base discord-haskell postgresql-simple
+  ];
   license = stdenv.lib.licenses.agpl3;
 }

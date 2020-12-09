996d0cb3445b1476f11f0c270d82cd8d6ad4b1ad
Dan Dart <git@dandart.co.uk>
2020-12-09 20:44:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# frugaled
+
+Frugal Eating Disorder App
+
+Aims:
+
+- Tell us what we have
+- Tell us what to eat quickly
+- Tell us what to buy more of
+- Ask us what kinds of things we feel like at the time
+- Tell us how much money we have
+- Tell us what we can/can't afford
+- Remind us to eat and ask us out of what's available, what we want
+- Allocate a food budget soft limit
+- Tell Dan what to make for Babygirl
+- Tell us when we've saved enough money to have a treat
+- Ask us what we've eaten
+- Allow us to tell it what we're running low on, and then guess based upon number of meals how much something will last us.
---
+++ sql/init.sql
@@ -0,0 +1,105 @@
+CREATE TABLE foodtypes (
+    id SERIAL NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE qtytypes (
+    id SERIAL NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE ingredients (
+    id SERIAL NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    mass_per_unit INT NOT NULL,
+    portions_per_unit INT NOT NULL,
+    foodtypeid SERIAL NOT NULL,
+    qtytypeid SERIAL NOT NULL,
+    cals_per_portion INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (foodtypeid) REFERENCES foodtypes(id),
+    FOREIGN KEY (qtytypeid) REFERENCES qtytypes(id)
+);
+
+CREATE TABLE stock (
+    id SERIAL NOT NULL UNIQUE,
+    qty INT NOT NULL,
+    expiry TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE recipes (
+    id SERIAL NOT NULL UNIQUE,
+    name VARCHAR(255),
+    instructions TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE recipe_ingredient (
+    id SERIAL NOT NULL UNIQUE,
+    recipeid SERIAL NOT NULL,
+    ingredientid SERIAL NOT NULL,
+    qty INT NOT NULL,
+    qtytypeid SERIAL NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (recipeid) REFERENCES recipes(id),
+    FOREIGN KEY (ingredientid) REFERENCES ingredients(id),
+    FOREIGN KEY (qtytypeid) REFERENCES qtytypes(id)
+);
+
+CREATE TABLE households (
+    id SERIAL NOT NULL UNIQUE,
+    name VARCHAR(255),
+    budget DECIMAL,
+    currentmoney DECIMAL,
+    payday INT,
+    discordgroupid VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE users (
+    id SERIAL NOT NULL UNIQUE,
+    name VARCHAR(255),
+    discorduserid VARCHAR(255),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE users_ingredients (
+    userid SERIAL NOT NULL UNIQUE,
+    ingredientid SERIAL NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (userid) REFERENCES users(id),
+    FOREIGN KEY (ingredientid) REFERENCES ingredients(id),
+    PRIMARY KEY (userid, ingredientid)
+);
+
+CREATE TABLE reminders (
+    id SERIAL NOT NULL UNIQUE,
+    userid SERIAL NOT NULL,
+    time TIME NOT NULL,
+    days INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (userid) REFERENCES users(id)
+);
